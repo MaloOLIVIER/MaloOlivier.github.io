948d080a4bc2ae47096a2d87d83e29338f4a9faa
MaloOLIVIER <MaloOLIVIER333@gmail.com>
2024-09-20 18:51:03 +0200

    fixed temporarily the Homepage cards bad display -- not coherent solution though

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -57,79 +57,90 @@ const Homepage: React.FC = () => {
                 </Grid>
             </Grid>
 
+            <Grid container spacing={2} columns={12}>
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <SyledCard
+                        variant="outlined"
+                        onClick={() => handleClick("https://arxiv.org/pdf/2407.11828")}
+                        onFocus={() => handleFocus(0)}
+                        onBlur={handleBlur}
+                        tabIndex={0}
+                        className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
+                    >
+                        <CardMedia
+                            component="img"
+                            alt="vibravox"
+                            image={cardData[0].img}
+                            aspect-ratio="16 / 9"
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                objectFit: 'contain', // Ensure the image fits within the container
+                                height: '100%', // Ensure the image takes the full height of the container
+                            }}
+                        />
 
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-                <Grid container spacing={2} columns={12}>
-                    <Grid size={{ xs: 12, md: 6 }}>
-                        <SyledCard
-                            variant="outlined"
-                            onClick={() => handleClick("https://arxiv.org/pdf/2407.11828")}
-                            onFocus={() => handleFocus(0)}
-                            onBlur={handleBlur}
-                            tabIndex={0}
-                            className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
-                        >
-                            <CardMedia
-                                component="img"
-                                alt="vibravox"
-                                image={cardData[0].img}
-                                aspect-ratio="16 / 9"
-                                sx={{
-                                    borderBottom: '1px solid',
-                                    borderColor: 'divider',
-                                }}
-                            />
-
-                            <SyledCardContent>
-                                <Typography gutterBottom variant="caption" component="div" fontFamily={'Roboto'}>
-                                    {cardData[0].tag}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="div" fontFamily={'Roboto'}>
-                                    {cardData[0].title}
-                                </Typography>
-                                <StyledTypography variant="body2" color="text.secondary" gutterBottom fontFamily={'Roboto'}>
-                                    {cardData[0].description}
-                                </StyledTypography>
-                            </SyledCardContent>
-                            <Author authors={cardData[0].authors} date={cardData[0].date} />
-                        </SyledCard>
-                    </Grid>
-                    <Grid size={{ xs: 12, md: 6 }}>
-                        <SyledCard
-                            variant="outlined"
-                            onClick={() => handleClick("/static/images/papers/MaloOLIVIER_MasterThesis.pdf")}
-                            onFocus={() => handleFocus(0)}
-                            onBlur={handleBlur}
-                            tabIndex={0}
-                            className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
-                        >
-                            <CardMedia
-                                component="img"
-                                alt="insa"
-                                image={cardData[1].img}
-                                aspect-ratio="16 / 9"
-                                sx={{
-                                    borderBottom: '1px solid',
-                                    borderColor: 'divider',
-                                }}
-                            />
-
-                            <SyledCardContent>
-                                <Typography gutterBottom variant="caption" component="div" fontFamily={'Roboto'}>
-                                    {cardData[1].tag}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="div" fontFamily={'Roboto'}>
-                                    {cardData[1].title}
-                                </Typography>
-                                <StyledTypography variant="body2" color="text.secondary" gutterBottom fontFamily={'Roboto'}>
-                                    {cardData[1].description}
-                                </StyledTypography>
-                            </SyledCardContent>
-                            <Author authors={cardData[1].authors} date={cardData[1].date} />
-                        </SyledCard>
-                    </Grid>
+                        <SyledCardContent sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'flex-end',
+                        }}>
+                            <Typography sx={{ borderTop: '1px solid', borderColor: 'divider' }} gutterBottom variant="caption" component="div" fontFamily={'Roboto'}>
+                                {cardData[0].tag}
+                            </Typography>
+                            <Typography gutterBottom variant="h6" component="div" fontFamily={'Roboto'}>
+                                {cardData[0].title}
+                            </Typography>
+                            <StyledTypography variant="body2" color="text.secondary" gutterBottom fontFamily={'Roboto'}>
+                                {cardData[0].description}
+                            </StyledTypography>
+                        </SyledCardContent>
+                        <Author authors={cardData[0].authors} date={cardData[0].date} />
+                    </SyledCard>
                 </Grid>
-            </Box>
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <SyledCard
+                        variant="outlined"
+                        onClick={() => handleClick("/static/images/papers/MaloOLIVIER_MasterThesis.pdf")}
+                        onFocus={() => handleFocus(0)}
+                        onBlur={handleBlur}
+                        tabIndex={0}
+                        className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
+                    >
+                        <CardMedia
+                            component="img"
+                            alt="insa"
+                            image={cardData[1].img}
+                            aspect-ratio="16 / 9"
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                objectFit: 'contain', // Ensure the image fits within the container
+                                height: '100%', // Ensure the image takes the full height of the container
+                            }}
+                        />
+
+                        <SyledCardContent sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'flex-end',
+                        }}>
+                            <Typography sx={{ borderTop: '1px solid', borderColor: 'divider' }} gutterBottom variant="caption" component="div" fontFamily={'Roboto'}>
+                                {cardData[1].tag}
+                            </Typography>
+                            <Typography gutterBottom variant="h6" component="div" fontFamily={'Roboto'}>
+                                {cardData[1].title}
+                            </Typography>
+                            <StyledTypography variant="body2" color="text.secondary" gutterBottom fontFamily={'Roboto'}>
+                                {cardData[1].description}
+                            </StyledTypography>
+                        </SyledCardContent>
+                        <Author authors={cardData[1].authors} date={cardData[1].date} />
+                    </SyledCard>
+                </Grid>
+            </Grid>
 
         </Container>
     );
